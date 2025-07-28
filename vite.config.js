df313ef4d572c8ba20e189c1a9fdd203faa25af2
王小可 <mortyestxie@gmail.com>
2025-07-29 03:38:14 +0800

    adjust the base url of this page

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/dissociative-disorder-page/',
+  base: '/',
   server: {
     host: '0.0.0.0', // 允许局域网访问
     port: 5174, // 固定端口
